ba59632ec909ebc21100dd1c80ec9558705abe4d
Bhawick Jain <bhawick@gmail.com>
2022-08-14 09:43:24 +0100

    routing and linking

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function FirstPost(): JSX.Element {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  );
+}
