a685b86a2b95ae7abcc0b49be9ac35adcc8ba0e4
Bhawick Jain <bhawick@gmail.com>
2022-09-05 20:04:22 +0100

    a mock blog site

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -7,7 +7,7 @@ import Layout from '../../components/Layout';
 export default function FirstPost(): JSX.Element {
   console.log('Hello First Post in the console')
   return (
-      <Layout>
+      <Layout home={false}>
         <>
           <Head>
             <title>First Post</title>
