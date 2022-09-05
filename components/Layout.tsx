745001870713512214337fedf17368dbfa89eb52
Bhawick Jain <bhawick@gmail.com>
2022-09-05 19:43:34 +0100

    using css modules and global css

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,15 +5,19 @@ interface LayoutProps {
 }
 
 /*
-To use this container class inside components/layout.js, you need to:
- - Import the CSS file and assign a name to it, like styles
- - Use styles.container as the className
-
- This is what CSS Modules does: It automatically generates unique
- class names. As long as you use CSS Modules, you don’t have to worry
- about class name collisions.
 */
 
+/**
+ * To use this container class inside components/layout.js, you need to:
+ * - Import the CSS file and assign a name to it, like styles
+ * - Use styles.container as the className
+ *
+ * This is what CSS Modules does: It automatically generates unique
+ * class names. As long as you use CSS Modules, you don’t have to worry
+ * about class name collisions.
+ * 
+ */
 export default function Layout({ children }: LayoutProps) {
     return <div className={styles.container}>{children}</div>
 }
+
