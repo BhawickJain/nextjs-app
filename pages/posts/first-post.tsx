231e5b8ecb2fefe8e2b73267431b5b5062fe0cf8
Bhawick Jain <bhawick@gmail.com>
2022-09-05 08:45:10 +0100

    use Scripts component for loading scripts

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,13 +1,22 @@
 import Image from 'next/image'
 import Link from "next/link";
 import Head from 'next/head';
+import Script from 'next/script'
 
 export default function FirstPost(): JSX.Element {
+  console.log('Hello First Post in the console')
   return (
     <>
       <Head>
         <title>First Post</title>
-         <script src="https://connect.facebook.net/en_US/sdk.js" />
+        {/* doesn't seem to work! */}
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        />
       </Head>
       <h1>First Post</h1>
       <h2>
