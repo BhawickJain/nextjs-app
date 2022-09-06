e200c7dc1e0f751fddf604a95b13ac6cdee4a546
Bhawick Jain <bhawick@gmail.com>
2022-09-06 18:55:51 +0100

    fix navigation issues

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 }
 
 module.exports = nextConfig
