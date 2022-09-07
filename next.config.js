18299c8ac3cbbce4eaee6817a16573ce5c998cdd
BhawickJain <bhawick@outlook.com>
2022-09-07 16:44:15 +0100

    load images unoptimised

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-const reponame = 'nextjs-app';
+const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
+const reponame = "nextjs-app";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images.unoptimized = true,
-  basePath: ghPages? reponame : '',
-  assetPrefix: ghPages ? reponame : '',
-}
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  basePath: ghPages ? reponame : "",
+  assetPrefix: ghPages ? reponame : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
