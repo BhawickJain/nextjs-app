db26f3e66bbdc3297f0bde217a3b4c99cd26d595
Bhawick Jain <bhawick@gmail.com>
2022-09-07 23:59:20 +0100

    remove forward slash

--- lib/cdnBasePath.ts
+++ lib/cdnBasePath.ts
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 // set CDN BASE PATH
-const cdnBasePath = ghPages ? 'https://bhawickjain.github.io/nextjs-app/' : ''
+const cdnBasePath = ghPages ? 'https://bhawickjain.github.io/nextjs-app' : ''
 
 export default cdnBasePath
