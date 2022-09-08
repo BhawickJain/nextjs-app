212fc3ae6fdf363f6ca733ef360483e822f5130b
Bhawick Jain <bhawick@gmail.com>
2022-09-08 20:58:57 +0100

    acess repo name with a custom env var name

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
-const repoName = process.env.GITHUB_REPOSITORY.matchAll(/(.*)\/(.*)/g)[0][2]
+const repoName = process.env.GITHUB_REPO.matchAll(/(.*)\/(.*)/g)[0][2]
 const prefixedPath = `/${repoName}`;
 
 /** @type {import('next').NextConfig} */
