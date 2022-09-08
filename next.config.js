104f157096dfdc1c5579b731191400704c310d1b
Bhawick Jain <bhawick@gmail.com>
2022-09-08 20:05:51 +0100

    listen for node production environment variable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-const isProd = process.env.DEPLOY_TARGET === "gh-pages";
+const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.DEPLOY_TARGET === "gh-pages";
 const repoName = "/nextjs-app";
 
 /** @type {import('next').NextConfig} */
