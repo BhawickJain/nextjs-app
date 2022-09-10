890ef44545b7e8fe019ac2f4877e476dc30b44f0
Bhawick Jain <bhawick@gmail.com>
2022-09-10 20:20:53 +0100

    remove posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,13 @@ import Link from "next/link";
 import Date from '../components/Date';
 import utilStyles from '../styles/utils.module.css';
 
-interface HomeProps {
-  allPostsData: IPost[]
-}
-
 /**
  * Note how the home attribute is present without a value 
  * within the Layout tag, this is to say that this Layout
  * is the Home page. There is conditional rendering within
  * the Layout component for a home page.
  */
-export default function Home( { allPostsData }: HomeProps ) {
+export default function Home() {
   return (
     <Layout home>
       <>
@@ -28,35 +24,11 @@ export default function Home( { allPostsData }: HomeProps ) {
           Welcome to the Next.js site, it is a sample typescript project with static prerendering.
           Markdown post have been prerendered into links and pages. 
           Below are some posts I have written!
+          
+          This is a single page app (SPA) site!
           </p>
-
-        </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Posts</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
-           ))}
-          </ul>
         </section>
       </>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
