d18593a2b92138fed1d43f40d6f2087cf6408d66
Bhawick Jain <bhawick@gmail.com>
2022-09-12 22:06:26 +0100

    fix #3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production'
 // solves issue #2
-const repoName = isProd ? Array.from(process.env.GITHUB_REPOSITORY.matchAll(/(.*)\/(.*)/g))[0][2] : undefined
+const repoName = isProd
+  ? process.env.GITHUB_REPOSITORY
+    ? Array.from(process.env.GITHUB_REPOSITORY.matchAll(/(.*)\/(.*)/g))[0][2]
+    : undefined
+  : undefined;
 const prefixedPath = isProd ? `/${repoName}`: undefined;
 
 /** @type {import('next').NextConfig} */
